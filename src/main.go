0b5df4290ba3ba870427487a320b73c618d489c0
Jia <jia.tian@me.com>
2019-07-02 15:57:05 +0800

    add geoip query api

--- src/main.go
+++ src/main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/apex/gateway"
 	"github.com/gin-gonic/gin"
+	"github.com/oschwald/geoip2-golang"
 )
 
 var (
@@ -109,12 +110,65 @@ func routerEngine() *gin.Engine {
 		}
 		c.JSON(http.StatusOK, resp)
 	})
+	r.GET("/geoip/:ip", func(c *gin.Context) {
+		ip := net.ParseIP(c.Param("ip"))
+		if ip == nil {
+			c.String(http.StatusBadRequest, "ip parse failed")
+			return
+		}
+		db, err := geoip2.Open("geoip/GeoLite2-City.mmdb")
+		if err != nil {
+			c.String(http.StatusGone, "geoip db read failed.")
+			log.Fatal(err)
+		}
+		defer db.Close()
+		// If you are using strings that may be invalid, check that ip is not nil
+		record, err := db.City(ip)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.ToLower(c.Query("view")) == "json" {
+			c.JSON(http.StatusOK, record)
+			return
+		}
+
+		lang := c.Query("lang")
+		if lang == "" {
+			lang = "en"
+		}
+
+		var result string
+		//city
+		result += fmt.Sprintf("City: %s\n", record.City.Names[lang])
+		//Subdivisions
+		if len(record.Subdivisions) > 0 {
+			result += fmt.Sprintf("Subdivisions: %s\n", record.Subdivisions[0].Names["en"])
+		}
+		//Country
+		result += fmt.Sprintf("Country: %s\n", record.Country.Names[lang])
+		//Continent
+		result += fmt.Sprintf("Continent: %s\n", record.Continent.Names[lang])
+		result += fmt.Sprintf("ISO country code: %v\n", record.Country.IsoCode)
+		result += fmt.Sprintf("Time zone: %v\n", record.Location.TimeZone)
+		result += fmt.Sprintf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
+		c.String(http.StatusOK, result)
+	})
 
 	return r
 }
 
 func main() {
-	addr := ":" + os.Getenv("PORT")
+	addr := ":8000"
+	if os.Getenv("PORT") != "" {
+		addr = ":" + os.Getenv("PORT")
+	}
+	if os.Getenv("simpleHTTP") != "" {
+		log.Println("listen http: ", addr)
+		log.Fatal(routerEngine().Run(addr))
+		return
+	}
+	log.Println("listen: ", addr)
 	log.Fatal(gateway.ListenAndServe(addr, routerEngine()))
 }
 
