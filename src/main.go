7f63ada17b487dd610601fdc5824f0d7ed087d3f
Jia <jia.tian@me.com>
2019-08-07 18:11:11 +0800

    NOT support application/octet-stream

--- src/main.go
+++ src/main.go
@@ -149,7 +149,6 @@ func routerEngine() *gin.Engine {
 			c.String(http.StatusBadRequest, "data size over limit")
 			return
 		}
-		log.Printf("in: %s\n", string(raw))
 		hash, err := storeSet(raw)
 		if err != nil {
 			c.String(http.StatusInternalServerError, err.Error())
@@ -174,7 +173,6 @@ func routerEngine() *gin.Engine {
 			c.String(http.StatusNotFound, "hash not found")
 			return
 		}
-		log.Printf("out: %s\n", string(value))
 		c.Data(http.StatusOK, "text/plain", value)
 	})
 
